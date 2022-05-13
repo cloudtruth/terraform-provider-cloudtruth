0e00076b82c1f4ab553e95eff26064878cb68d99
Matthew Cummings <matthewcummings516@gmail.com>
2022-05-12 22:57:23 -0500

    s/scaffolding/cloudtruth/ part 1

--- main.go
+++ main.go
@@ -7,16 +7,6 @@ import (
 	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
-
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
@@ -35,8 +25,7 @@ func main() {
 	opts := &plugin.ServeOpts{
 		Debug: debugMode,
 
-		// TODO: update this string with the full name of your provider as used in your configs
-		ProviderAddr: "registry.terraform.io/hashicorp/scaffolding",
+		ProviderAddr: "registry.terraform.io/terraform-provider-cloudtruth/cloudtruth",
 
 		ProviderFunc: provider.New(version),
 	}
