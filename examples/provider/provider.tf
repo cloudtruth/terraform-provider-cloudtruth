c5660500f7062b137d456fd69b6f6ff6c55ae2bb
Matthew Cummings <matthewcummings516@gmail.com>
2022-05-14 17:36:32 -0500

    Local provider terraform init-ing now

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,8 +1,15 @@
+# The path convention for providers is:
+# HOSTNAME/NAMESPACE/TYPE/terraform-provider-TYPE_VERSION_TARGET.zip
+# In our case: registry.terraform.io/cloudtruth/cloudtruth
+# This does not look redundant when the company who owns the provider
+# is not the same as the service the provider is build for e.g.
+# registry.terraform.io/hashicorp/aws
 terraform {
   required_version = ">=0.13.0"
 
   required_providers {
     cloudtruth = {
+      source = "cloudtruth/cloudtruth"
       version = "0.0.1"
     }
 
