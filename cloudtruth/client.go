9a7f2b68c81c6f97ff705c16eefc967f953b200a
Matthew Cummings <matthewcummings516@gmail.com>
2022-05-23 00:38:29 -0500

    Remove sync code + set provider level env vars for acceptance tests

--- cloudtruth/client.go
+++ cloudtruth/client.go
@@ -6,12 +6,10 @@ import (
 	"fmt"
 	"github.com/cloudtruth/terraform-provider-cloudtruth/pkg/cloudtruthapi"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	//"sync"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 )
 
 // todo:
-// set the default env and project
-// read Parameter -> data source, determine what properties to expose in HCL
 // read Parameters based on search/filter, but scoped to one env and project
 // project CRUD
 // env CRUD
@@ -19,13 +17,64 @@ import (
 // template CRUD
 // import support for all resources
 
-/* sync.Once can only be used one time per function
-var (
-	projNameCacheOnce sync.Once
-	projIDCacheOnce   sync.Once
-	envNameCacheOnce  sync.Once
-	envIDCacheOnce    sync.Once
-        )*/
+const (
+	// Allow for CloudTruth CLI API key definition in addition to TF_VAR_var_name approach
+	// no default
+	apiKeyVarName = "CLOUDTRUTH_API_KEY"
+
+	// Optional variables with default values, settable/overridable per Terraform state
+	domainVarName   = "CLOUDTRUTH_DOMAIN"
+	defaultDomain   = "api.cloudtruth.io"
+	protocolVarName = "CLOUDTRUTH_PROTOCOL"
+	defaultProtocol = "https"
+
+	// Optional config variables with no defaults - settable/overridable per Terraform state
+	projectVarName     = "CLOUDTRUTH_PROJECT"
+	environmentVarName = "CLOUDTRUTH_ENVIRONMENT"
+)
+
+type clientConfig struct {
+	APIKey      string
+	Project     string
+	Environment string
+	UserAgent   string
+	Domain      string
+	Protocol    string
+}
+
+func configureClient(ctx context.Context, conf clientConfig) (*cloudTruthClient, diag.Diagnostics) {
+	tflog.Debug(ctx, "configureClient")
+	tflog.Debug(ctx, fmt.Sprintf("%+v", conf))
+	apiConfig := cloudtruthapi.NewConfiguration()
+	apiConfig.Host = conf.Domain
+	apiConfig.Scheme = conf.Protocol
+	apiConfig.AddDefaultHeader("Authorization", fmt.Sprintf("Api-Key %s", conf.APIKey))
+	apiConfig.AddDefaultHeader("UserAgent", conf.UserAgent)
+	client := cloudTruthClient{
+		config:        conf,
+		openAPIClient: cloudtruthapi.NewAPIClient(apiConfig),
+	}
+
+	// load caches
+	err := client.loadProjectNameCache(ctx)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+	err = client.loadProjectIDCache(ctx)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+	err = client.loadEnvNameCache(ctx)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+	err = client.loadEnvIDCache(ctx)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+
+	return &client, nil
+}
 
 type cloudTruthClient struct {
 	config        clientConfig
