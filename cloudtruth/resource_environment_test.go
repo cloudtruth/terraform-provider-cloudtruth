1728a5663bfdef2f04528eb148b21f0dce279cdb
Matthew Cummings <matthewcummings516@gmail.com>
2022-06-03 21:15:58 -0500

    force_delete for environments + tests

--- cloudtruth/resource_environment_test.go
+++ cloudtruth/resource_environment_test.go
@@ -3,6 +3,7 @@ package cloudtruth
 import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"strconv"
 	"testing"
 )
 
@@ -11,7 +12,7 @@ const updateEnvDesc = "A new description of an environment"
 
 // todo: add a non-default parent test
 // and re-parent test if applicable
-func TestAccResourceEnvironmentBasic(t *testing.T) {
+func TestAccResourceEnvBasic(t *testing.T) {
 	createEnvName := fmt.Sprintf("TestEnv-%s", resource.UniqueId())
 	updateEnvName := fmt.Sprintf("updated-%s", createEnvName)
 	resource.Test(t, resource.TestCase{
@@ -19,14 +20,14 @@ func TestAccResourceEnvironmentBasic(t *testing.T) {
 		PreCheck:          func() { testAccPreCheck(t) },
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceEnvironmentCreateBasic(createEnvName, envdesc),
+				Config: testAccResourceEnvCreateBasic(createEnvName, envdesc),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("cloudtruth_environment.basic", "name", createEnvName),
 					resource.TestCheckResourceAttr("cloudtruth_environment.basic", "description", envdesc),
 				),
 			},
 			{
-				Config: testAccResourceEnvironmentUpdateBasic(updateEnvName, updateEnvDesc),
+				Config: testAccResourceEnvUpdateBasic(updateEnvName, updateEnvDesc),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("cloudtruth_environment.basic", "name", updateEnvName),
 					resource.TestCheckResourceAttr("cloudtruth_environment.basic", "description", updateEnvDesc),
@@ -36,7 +37,39 @@ func TestAccResourceEnvironmentBasic(t *testing.T) {
 	})
 }
 
-func testAccResourceEnvironmentCreateBasic(envName, desc string) string {
+func TestAccResourceEnvForceDelete(t *testing.T) {
+	forceDeleteEnvName := fmt.Sprintf("TestDeleteEnv-%s", resource.UniqueId())
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		// todo: uncomment when https://github.com/hashicorp/terraform-plugin-sdk/pull/976 has been merged
+		// and allows us to confirm that the delete indeed fails, for now we
+		//ExpectDestroyError: regexp.MustCompile(`.*cannot be deleted from the CloudTruth provider, you must enable 'force_delete' to allow this deletes.*`),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceEnvForceDeleteDisabled(forceDeleteEnvName, envdesc),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("cloudtruth_environment.force_delete", "name", forceDeleteEnvName),
+					resource.TestCheckResourceAttr("cloudtruth_environment.force_delete", "description", envdesc),
+					resource.TestCheckResourceAttr("cloudtruth_environment.force_delete", "force_delete",
+						strconv.FormatBool(false)),
+				),
+			},
+			{ // todo: move this to a new Test(Step) when we can validate that the above delete fails
+				// keeping it here now ensures that temporary resource cleanup will happen
+				Config: testAccResourceEnvForceDeleteEnabled(forceDeleteEnvName, envdesc),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("cloudtruth_environment.force_delete", "name", forceDeleteEnvName),
+					resource.TestCheckResourceAttr("cloudtruth_environment.force_delete", "description", envdesc),
+					resource.TestCheckResourceAttr("cloudtruth_environment.force_delete", "force_delete",
+						strconv.FormatBool(true)),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceEnvCreateBasic(envName, desc string) string {
 	return fmt.Sprintf(`
 	resource "cloudtruth_environment" "basic" {
   		name         = "%s"
@@ -46,12 +79,31 @@ func testAccResourceEnvironmentCreateBasic(envName, desc string) string {
 	`, envName, desc)
 }
 
-func testAccResourceEnvironmentUpdateBasic(envName, desc string) string {
+func testAccResourceEnvUpdateBasic(envName, desc string) string {
 	return fmt.Sprintf(`
 	resource "cloudtruth_environment" "basic" {
   		name         = "%s"
   		description  = "%s"
         force_delete = true
+	}
+	`, envName, desc)
+}
+
+func testAccResourceEnvForceDeleteDisabled(envName, desc string) string {
+	return fmt.Sprintf(`
+	resource "cloudtruth_environment" "force_delete" {
+  		name         = "%s"
+  		description  =  "%s"
+	}
+	`, envName, desc)
+}
+
+func testAccResourceEnvForceDeleteEnabled(envName, desc string) string {
+	return fmt.Sprintf(`
+	resource "cloudtruth_environment" "force_delete" {
+  		name         = "%s"
+  		description  =  "%s"
+		force_delete = true
 	}
 	`, envName, desc)
 }
--- cloudtruth/resource_project_test.go
+++ cloudtruth/resource_project_test.go
@@ -47,20 +47,26 @@ func TestAccResourceProjectForceDelete(t *testing.T) {
 		ProviderFactories: testProviderFactories,
 		PreCheck:          func() { testAccPreCheck(t) },
 		// todo: uncomment when https://github.com/hashicorp/terraform-plugin-sdk/pull/976 has been merged
+		// and allows us to confirm that the delete indeed fails, for now we
 		//ExpectDestroyError: regexp.MustCompile(`.*cannot be deleted from the CloudTruth provider, you must enable 'force_delete' to allow this deletes.*`),
 		Steps: []resource.TestStep{
-			{ // The delete should fail
+			{
 				Config: testAccResourceProjectForceDeleteDisabled(forceDeleteProjName, desc),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("cloudtruth_project.force_delete", "name", forceDeleteProjName),
 					resource.TestCheckResourceAttr("cloudtruth_project.force_delete", "description", desc),
+					resource.TestCheckResourceAttr("cloudtruth_project.force_delete", "force_delete",
+						strconv.FormatBool(false)),
 				),
 			},
-			{ // Then set force_delete = true and there should be no errors
+			{ // todo: move this to a new Test(Step) when we can validate that the above delete fails
+				// keeping it here now ensures that temporary resource cleanup will happen
 				Config: testAccResourceProjectForceDeleteEnabled(forceDeleteProjName, desc),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("cloudtruth_project.force_delete", "name", forceDeleteProjName),
 					resource.TestCheckResourceAttr("cloudtruth_project.force_delete", "description", desc),
+					resource.TestCheckResourceAttr("cloudtruth_project.force_delete", "force_delete",
+						strconv.FormatBool(true)),
 				),
 			},
 		},
