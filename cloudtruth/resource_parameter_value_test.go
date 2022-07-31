eb3ca9d8de41dbc29d3edecc77b8747ddf056238
Matthew Cummings <matthewcummings516@gmail.com>
2022-07-31 06:16:44 -0400

    Parameter + parameter values tests updated

--- cloudtruth/resource_parameter_value_test.go
+++ cloudtruth/resource_parameter_value_test.go
@@ -1,50 +1,87 @@
 package cloudtruth
 
-/*
 import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"strconv"
 	"testing"
 )
 
-func TestAccResourceParameterValueBasic(t *testing.T) {
+func TestAccResourceParameterProdOverride(t *testing.T) {
 	createParamName := fmt.Sprintf("Test-%s", uuid.New().String())
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: testProviderFactories,
 		PreCheck:          func() { testAccPreCheck(t) },
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceParameterCreateBasic(accTestProject, createParamName, paramDesc, true),
+				Config: testAccResourceParameterCreateProdOverride(createParamName, createParamName, prodParamVal),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "name", createParamName),
-					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "description", paramDesc),
-					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "secret",
-						strconv.FormatBool(true)),
-				),
-			},
-			{
-				Config: testAccResourceParameterCreateBasic(accTestProject, createParamName, updateParamDesc, false),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "name", createParamName),
-					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "description", updateParamDesc),
-					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "secret",
-						strconv.FormatBool(false)),
+					resource.TestCheckResourceAttr("cloudtruth_parameter_value.prod_override", "parameter_name", createParamName),
+					resource.TestCheckResourceAttr("cloudtruth_parameter_value.prod_override", "value", prodParamVal),
+					resource.TestCheckResourceAttr("cloudtruth_parameter_value.prod_override", "environment", "production"),
 				),
 			},
 		},
 	})
 }
 
-func testAccResourceParameterCreateBasic(projName, paramName, desc string, isSecret bool) string {
+func TestAccResourceParameterExternal(t *testing.T) {
+	createParamName := fmt.Sprintf("Test-%s", uuid.New().String())
+	resource.Test(t, resource.TestCase{
+		ProviderFactories:         testProviderFactories,
+		PreventPostDestroyRefresh: true,
+		PreCheck:                  func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceParameterCreateExternal(createParamName, paramDesc, createParamName, githubLocation, githubFilter),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("cloudtruth_parameter_value.external", "parameter_name", createParamName),
+					resource.TestCheckResourceAttr("cloudtruth_parameter_value.external", "location", githubLocation),
+					resource.TestCheckResourceAttr("cloudtruth_parameter_value.external", "filter", githubFilter),
+				),
+			},
+		},
+	})
+}
+
+// This is a case where we create a parameter using the same name but overriding it in the
+// production environment i.e. not inheriting from the default environment
+func testAccResourceParameterCreateProdOverride(name, paramName, prodValue string) string {
 	return fmt.Sprintf(`
 	resource "cloudtruth_parameter" "basic" {
-		project     = "%s"
+        project     = "AcceptanceTest"
+  		name        = "%s"
+  		description = "Just a description of a parameter"
+	}
+
+	resource "cloudtruth_parameter_value" "prod_override" {
+        environment    = "production"
+  		parameter_name = "%s"
+        value          = "%s"
+        depends_on = [
+        	cloudtruth_parameter.basic
+        ]
+	}
+	`, name, paramName, prodValue)
+}
+
+func testAccResourceParameterCreateExternal(name, desc, paramName, location, filter string) string {
+	return fmt.Sprintf(`
+	resource "cloudtruth_parameter" "external" {
+        project     = "AcceptanceTest"
   		name        = "%s"
   		description = "%s"
-		secret      = "%t"
 	}
-	`, projName, paramName, desc, isSecret)
+
+	resource "cloudtruth_parameter_value" "external" {
+		project          = "AcceptanceTest"
+  		parameter_name   = "%s"
+        external         = true
+        location         = "%s"
+        filter           = "%s"
+        depends_on = [
+        	cloudtruth_parameter.external
+        ]
+	}
+	`, name, desc, paramName, location, filter)
 }
-*/
