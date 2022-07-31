33fb672705323379d5790f8fca1e4813ba2b9875
Matthew Cummings <matthewcummings516@gmail.com>
2022-07-31 05:09:04 -0400

    Update parameter value data sources

---
+++ cloudtruth/resource_parameter_value_test.go
@@ -0,0 +1,50 @@
+package cloudtruth
+
+/*
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"strconv"
+	"testing"
+)
+
+func TestAccResourceParameterValueBasic(t *testing.T) {
+	createParamName := fmt.Sprintf("Test-%s", uuid.New().String())
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceParameterCreateBasic(accTestProject, createParamName, paramDesc, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "name", createParamName),
+					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "description", paramDesc),
+					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "secret",
+						strconv.FormatBool(true)),
+				),
+			},
+			{
+				Config: testAccResourceParameterCreateBasic(accTestProject, createParamName, updateParamDesc, false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "name", createParamName),
+					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "description", updateParamDesc),
+					resource.TestCheckResourceAttr("cloudtruth_parameter.basic", "secret",
+						strconv.FormatBool(false)),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceParameterCreateBasic(projName, paramName, desc string, isSecret bool) string {
+	return fmt.Sprintf(`
+	resource "cloudtruth_parameter" "basic" {
+		project     = "%s"
+  		name        = "%s"
+  		description = "%s"
+		secret      = "%t"
+	}
+	`, projName, paramName, desc, isSecret)
+}
+*/
