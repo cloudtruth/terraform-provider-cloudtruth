cc11c52e5b8dbb3a25d1b84536aed088afeffd70
Matthew Cummings <matthewcummings516@gmail.com>
2023-02-23 23:53:15 -0500

    Run prod tests even if staging tests fail

--- scripts/cloudtruth_acc_test_env/terraform.tf
+++ scripts/cloudtruth_acc_test_env/terraform.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     cloudtruth = {
-      source = "cloudtruth/cloudtruth"
+      source  = "cloudtruth/cloudtruth"
       version = ">= 0.5.2"
     }
   }
 }
 
 provider "cloudtruth" {
-  domain = "api.staging.cloudtruth.io"
+  domain  = "api.staging.cloudtruth.io"
   project = "AcceptanceTest"
 }
 
