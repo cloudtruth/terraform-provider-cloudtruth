d36bd29521b369be29de39b0b0facd072b389793
Matthew Cummings <matthewcummings516@gmail.com>
2024-10-04 22:51:16 -0400

    Revert "Quick sanity check to repro the issue in the updated tests"
    This reverts commit a0aa731c14d901570f516ebe0b1776cd035f97e7.

--- cloudtruth/resource_aws_push_action.go
+++ cloudtruth/resource_aws_push_action.go
@@ -310,8 +310,7 @@ func resourceAWSPushActionUpdate(ctx context.Context, d *schema.ResourceData, me
 	}
 
 	if d.HasChange("projects") {
-		// TEMP rawProjects := d.Get("projects").(*schema.Set).List()
-		rawProjects := d.Get("projects").([]interface{})
+		rawProjects := d.Get("projects").(*schema.Set).List()
 		projects, err := getProjectURLs(ctx, c, rawProjects)
 		if err != nil {
 			return diag.FromErr(err)
